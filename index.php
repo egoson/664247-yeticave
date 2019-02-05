192c6e8938a6fa544c3023465f07e747dcc76b72
Денис <egoson@mail.ru>
2019-02-05 13:31:19 +0400

    поправил имя переменной

--- index.php
+++ index.php
@@ -92,9 +92,9 @@ $announcement = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <?php
-            foreach ($announcement as $key => $value): ?>
+            foreach ($announcement as $item): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$value["category"];?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$item["category"];?></a>
             </li>
             <?php endforeach; ?>
         </ul>
@@ -104,17 +104,17 @@ $announcement = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($announcement as $key => $value): ?>
+            <?php foreach ($announcement as $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src=<?=$value["url"];?> width="350" height="260" alt="">
+                    <img src=<?=$item["url"];?> width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$value["category"];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value["name"];?></a></h3>
+                    <span class="lot__category"><?=$item["category"];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item["name"];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount"><?=$value["price"];?></span>
+                            <span class="lot__amount"><?=$item["price"];?></span>
                             <span class="lot__cost">цена<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
