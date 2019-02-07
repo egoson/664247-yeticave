2f0b852fc9a2d37f876fa3733877f1d45f9392a7
Денис <egoson@mail.ru>
2019-02-07 15:05:58 +0400

    откат до читабельного кода

--- index.php
+++ index.php
@@ -44,7 +44,9 @@ $announcement = [
 ];
 function do_price($price)
 {
-    return number_format(ceil($price), 0, ',', ' ') . " <b class=\"rub\">р</b>";
+    $integer_price = ceil($price);
+    $integer_price = number_format($integer_price, 0, ',', ' ');
+    return $integer_price . " <b class=\"rub\">р</b>";
 };
 ?>
 <!DOCTYPE html>
@@ -119,8 +121,7 @@ function do_price($price)
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount"><?=$item["price"];?></span>
-                            <span class="lot__cost">
-                                <?=(do_price($item["price"])); ?></span>
+                            <span class="lot__cost"><?=(do_price($item["price"])); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
