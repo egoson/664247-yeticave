f3123021400b3a2b6b0b4cf2ac7c7b762bc839b9
Денис <egoson@mail.ru>
2019-02-12 19:56:31 +0400

    поправил по замечаниям

--- schema.sql
+++ schema.sql
@@ -2,15 +2,15 @@ CREATE DATABASE yeticave
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 USE yeticave;
-CREATE TABLE ussers (
+CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email CHAR(128) NOT NULL UNIQUE,
 	dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	name CHAR(50) NOT NULL,
-	contacts CHAR(11) NOT NULL UNIQUE,
+	contacts CHAR(11) NOT NULL,
 	avatar TINYTEXT,
-	lot_id CHAR(20),
-	rate_id CHAR(20)
+	lot_id INT(20),
+	rate_id INT(20)
 );
 
 CREATE TABLE categories (
@@ -18,26 +18,24 @@ CREATE TABLE categories (
 	name CHAR(128)
 );
 
-INSERT INTO categories (name) VALUES ("Доски и лыжи"), ("Крепления"), ("Ботинки"), ("Одежда"), ("Инструменты"), ("Разное");
-
 CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
 	dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	name CHAR(128),
 	description TINYTEXT,
-	link TINYTEXT,
-	start_price CHAR(20),
+	url TINYTEXT,
+	start_price INT(20),
 	dt_close TIMESTAMP,
-	step_price CHAR(20),
-	users_id CHAR(20),
-	win_id CHAR(20),
-	categories CHAR(20)
+	step_price INT(20),
+	users_id INT(20),
+	win_id INT(20),
+	categories_id INT(20)
 );
 
 CREATE TABLE rate (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	amount CHAR(20),
-	users_id CHAR(20),
-	lot_id CHAR(20)
+	users_id INT(20),
+	lot_id INT(20)
 );
